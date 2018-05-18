054d317e1f8eacf785641337ff6396f6767dc1ea
dustinpfister <dustin.pfister@gmail.com>
2018-05-18 10:22:09 -0400

    using _.filter with plan objects

--- demos/filter.js
+++ demos/filter.js
@@ -1,56 +1,120 @@
-var data = [
-    {post: 'lodash_isarray', clicks: 0, cat:'lodash'},
-    {post: 'lodash-find', clicks: 1668, cat: 'lodash'},
-    {post: 'lodash_map', clicks: 338, cat:'lodash'},
-    {post: 'lodash_merge', clicks: 13, cat:'lodash'},
-    {post: 'phaser-gameobj-input-keyboard', clicks: 118, cat: 'phaser'},
-    {post: 'phaser-state-manager', clicks: 30, cat: 'phaser'},
-    {post: 'phaser-graphics', clicks: 0, cat: 'phaser'},
-    {post: 'phaser-getting-started', clicks: 4, cat: 'phaser'},
-    {post: 'nodejs-axios', clicks: 577, cat: 'nodejs'},
-    {post: 'nodejs-marked', clicks: 73, cat: 'nodejs'},
-    {post: 'nodejs-jimp', clicks: 145, cat: 'nodejs'},
-    {post: 'nodejs-harp', clicks: 1, cat: 'nodejs'},
+var data = [{
+        post: 'lodash_isarray',
+        clicks: 0,
+        cat: 'lodash'
+    }, {
+        post: 'lodash-find',
+        clicks: 1668,
+        cat: 'lodash'
+    }, {
+        post: 'lodash_map',
+        clicks: 338,
+        cat: 'lodash'
+    }, {
+        post: 'lodash_merge',
+        clicks: 13,
+        cat: 'lodash'
+    }, {
+        post: 'phaser-gameobj-input-keyboard',
+        clicks: 118,
+        cat: 'phaser'
+    }, {
+        post: 'phaser-state-manager',
+        clicks: 30,
+        cat: 'phaser'
+    }, {
+        post: 'phaser-graphics',
+        clicks: 0,
+        cat: 'phaser'
+    }, {
+        post: 'phaser-getting-started',
+        clicks: 4,
+        cat: 'phaser'
+    }, {
+        post: 'nodejs-axios',
+        clicks: 577,
+        cat: 'nodejs'
+    }, {
+        post: 'nodejs-marked',
+        clicks: 73,
+        cat: 'nodejs'
+    }, {
+        post: 'nodejs-jimp',
+        clicks: 145,
+        cat: 'nodejs'
+    }, {
+        post: 'nodejs-harp',
+        clicks: 1,
+        cat: 'nodejs'
+    },
 ];
 
 // using lodash _.filter to get posts that are doing good
 var onTarget = _.filter(data, function (rec) {
 
-    // return posts that have 280 or more clicks
-    return rec.clicks >= 280;
+        // return posts that have 280 or more clicks
+        return rec.clicks >= 280;
 
-});
-
+    });
 
 console.log(JSON.stringify(onTarget));
 /*
-[
-    {"post":"lodash-find","clicks":1668,"cat":"lodash"},
-    {"post":"lodash_map","clicks":338,"cat":"lodash"},
-    {"post":"nodejs-axios","clicks":577,"cat":"nodejs"}
+[{"post":"lodash-find","clicks":1668,"cat":"lodash"},{"post":"lodash_map","clicks":338,"cat":"lodash"},{"post":"nodejs-axios","clicks":577,"cat":"nodejs"}
 ]
-*/
+ */
 
-// vanilla js
+// VANILLA JS Array.filter
 var onTargetVS = data.filter(function (rec) {
 
-    // return posts that have 100 or more clicks
-    // but less than 1000
-    return rec.clicks >= 280;
+        // return posts that have 100 or more clicks
+        // but less than 1000
+        return rec.clicks >= 280;
 
-});
+    });
 
 console.log(JSON.stringify(onTargetVS));
 
-
 // BAKED IN ITERATEE SHORTHANDS
-console.log( _.filter(data,_.matches({cat:'lodash'})) );
+console.log(_.filter(data, _.matches({
+            cat: 'lodash'
+        })));
 
 // use can use lodash iteratee shorthands with
 // the native Array.filter method
-console.log( data.filter( _.matches({cat:'lodash'})) );
+console.log(data.filter(_.matches({
+            cat: 'lodash'
+        })));
 
 // but the actual _.filter method has it baked in
-console.log(  _.filter(data,{cat:'lodash'}) );
+console.log(_.filter(data, {
+        cat: 'lodash'
+    }));
 
+// Plan Objects
+
+var obj = {
+
+    foo: 'bar',
+	bool: false,
+    n: 42,
+    c: 7
+
+};
+
+var numbers = _.filter(obj, function (val, key, obj) {
+
+        return typeof val === 'number';
+
+    });
+
+console.log(numbers); [42,7];
+
+// Vanilla js Array.filter will not just work on any object
+var numbers = [].filter.call(obj, function(val,key,obj){
+
+    return typeof val === 'number';
+
+});
+
+console.log(numbers); [];
 
